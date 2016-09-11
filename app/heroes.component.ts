1e3ad30d6fb01d3b14ca210222e772873cfaf91d
Alberto Castro <albertc87@gmail.com>
2016-09-11 17:17:06 -0500

    Last chapter: Http

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
     selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
@@ -12,7 +13,7 @@ export class HeroesComponent implements OnInit{
     selectedHero: Hero;
     heroes :Hero[];
     
-    constructor(private heroService:HeroService){}
+    constructor(private router:Router,private heroService:HeroService){}
 
     ngOnInit(): void{
         this.getHeroes();
@@ -23,5 +24,27 @@ export class HeroesComponent implements OnInit{
     }
     onSelect(hero: Hero):void{
         this.selectedHero = hero;
+    }
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+    add(name: string):void{
+      name = name.trim();
+      if(!name){return;}
+      this.heroService.create(name)
+        .then(hero =>{
+          this.heroes.push(hero);
+          this.selectedHero = null;  
+          });
+    }
+    delete(hero:Hero):void{
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h!== hero);
+                if(this.selectedHero === hero){
+                    this.selectedHero = null;
+                }
+            })
     }
 }
